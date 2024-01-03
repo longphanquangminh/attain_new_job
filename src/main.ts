be2f48bf49ded391b511f1b9e10c32c91f0854aa
longphanquangminh <lordaizensousuke2001@gmail.com>
2024-01-03 10:31:23 +0700

    Update common

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.use(express.static('.'));
+  const config = new DocumentBuilder()
+    .setTitle('ATTAIN NEW JOB project')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+  await app.listen(8080);
 }
 bootstrap();
