7949c44b8f13d67bd2f0df21bdc14e40ff907ff8
longphanquangminh <lordaizensousuke2001@gmail.com>
2024-01-07 23:40:59 +0700

    up @ApiProperty()

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,7 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateCommentDto {
+  @ApiProperty()
   ma_cong_viec: number;
   ma_nguoi_binh_luan: number;
   ngay_binh_luan: Date | string;
+  @ApiProperty()
   noi_dung: string;
+  @ApiProperty()
   sao_binh_luan: number;
 }
